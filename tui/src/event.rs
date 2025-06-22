35594cc55ed1d0fe164a7e5fa31baf5f91470d9b
mstfash <m.ashraf.hegab@gmail.com>
2025-06-22 21:21:21 +0300

    adding keyboard shortcuts

--- tui/src/event.rs
+++ tui/src/event.rs
@@ -1,31 +1,72 @@
 use crate::app::InputEvent;
-use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers, MouseEventKind};
+use crossterm::event::{Event, KeyCode, KeyEventKind, KeyModifiers, MouseEventKind};
 
 pub fn map_crossterm_event_to_input_event(event: Event) -> Option<InputEvent> {
     match event {
-        Event::Key(KeyEvent {
-            code, modifiers, ..
-        }) => match code {
-            KeyCode::Char('c') if modifiers.contains(KeyModifiers::CONTROL) => {
-                Some(InputEvent::Quit)
+        Event::Key(key) => {
+            if key.kind != KeyEventKind::Press {
+                return None;
             }
-            KeyCode::Char('j') if modifiers.contains(KeyModifiers::CONTROL) => {
-                Some(InputEvent::InputChangedNewline)
+            match key.code {
+                KeyCode::Char('c') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                    Some(InputEvent::Quit)
+                }
+                KeyCode::Char('j') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                    Some(InputEvent::InputChangedNewline)
+                }
+                KeyCode::Char('u') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                    Some(InputEvent::InputDelete)
+                }
+                KeyCode::Char('w') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                    Some(InputEvent::InputDeleteWord)
+                }
+                KeyCode::Char('a') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                    Some(InputEvent::InputCursorStart)
+                }
+                KeyCode::Char('e') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                    Some(InputEvent::InputCursorEnd)
+                }
+                KeyCode::Char('f') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                    Some(InputEvent::CursorRight)
+                }
+                KeyCode::Char('b') if key.modifiers.contains(KeyModifiers::CONTROL) => {
+                    Some(InputEvent::CursorLeft)
+                }
+                KeyCode::Char('!') => Some(InputEvent::ShellMode),
+                KeyCode::Char(c) => Some(InputEvent::InputChanged(c)),
+                KeyCode::Backspace => {
+                    if key.modifiers.contains(KeyModifiers::CONTROL) {
+                        Some(InputEvent::InputDelete)
+                    } else {
+                        Some(InputEvent::InputBackspace)
+                    }
+                }
+                KeyCode::Enter => Some(InputEvent::InputSubmitted),
+                KeyCode::Esc => Some(InputEvent::HandleEsc),
+                KeyCode::Up => Some(InputEvent::Up),
+                KeyCode::Down => Some(InputEvent::Down),
+                KeyCode::Left => {
+                    if key.modifiers.contains(KeyModifiers::CONTROL) {
+                        Some(InputEvent::InputCursorPrevWord)
+                    } else {
+                        Some(InputEvent::CursorLeft)
+                    }
+                }
+                KeyCode::Right => {
+                    if key.modifiers.contains(KeyModifiers::CONTROL) {
+                        Some(InputEvent::InputCursorNextWord)
+                    } else {
+                        Some(InputEvent::CursorRight)
+                    }
+                }
+                KeyCode::Home => Some(InputEvent::InputCursorStart),
+                KeyCode::End => Some(InputEvent::InputCursorEnd),
+                KeyCode::PageUp => Some(InputEvent::PageUp),
+                KeyCode::PageDown => Some(InputEvent::PageDown),
+                KeyCode::Tab => Some(InputEvent::Tab),
+                _ => None,
             }
-            KeyCode::Char('!') => Some(InputEvent::ShellMode),
-            KeyCode::Char(c) => Some(InputEvent::InputChanged(c)),
-            KeyCode::Backspace => Some(InputEvent::InputBackspace),
-            KeyCode::Enter => Some(InputEvent::InputSubmitted),
-            KeyCode::Esc => Some(InputEvent::HandleEsc),
-            KeyCode::Up => Some(InputEvent::Up),
-            KeyCode::Down => Some(InputEvent::Down),
-            KeyCode::Left => Some(InputEvent::CursorLeft),
-            KeyCode::Right => Some(InputEvent::CursorRight),
-            KeyCode::PageUp => Some(InputEvent::PageUp),
-            KeyCode::PageDown => Some(InputEvent::PageDown),
-            KeyCode::Tab => Some(InputEvent::Tab),
-            _ => None,
-        },
+        }
         Event::Mouse(me) => match me.kind {
             MouseEventKind::ScrollUp => Some(InputEvent::ScrollUp),
             MouseEventKind::ScrollDown => Some(InputEvent::ScrollDown),
