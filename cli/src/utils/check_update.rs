f81b240acfa293df9d7a03898d6ed80dcce7ebf8
Mostafa Ashraf <m.ashraf.hegab@gmail.com>
2025-07-01 07:25:29 +0300

    Logic to auto update using brew and binary

--- cli/src/utils/check_update.rs
+++ cli/src/utils/check_update.rs
@@ -2,6 +2,7 @@ use reqwest::header::{HeaderMap, HeaderValue, USER_AGENT};
 use serde::Deserialize;
 use std::error::Error;
 
+use crate::commands::auto_update::run_auto_update;
 #[derive(Deserialize, Debug)]
 pub struct Release {
     pub tag_name: String,
@@ -39,11 +40,38 @@ pub async fn get_latest_cli_version() -> Result<String, Box<dyn Error>> {
     let url = "https://api.github.com/repos/stakpak/cli/releases/latest".to_string();
 
     let response = client.get(&url).send().await?;
-
+    
     if !response.status().is_success() {
         return Err("Failed to fetch release info".into());
     }
 
     let release: Release = response.json().await?;
     Ok(release.tag_name)
+}
+
+pub async fn auto_update() -> Result<(), Box<dyn Error>> {
+    let latest_version = get_latest_cli_version().await?;
+    let current_version = format!("v{}", env!("CARGO_PKG_VERSION"));
+    if current_version != latest_version {
+        println!(
+            "\n🚀 Update available!  \x1b[1;37m\x1b[1;33m{}\x1b[0m → \x1b[1;32m{}\x1b[0m ✨\n",
+            current_version, latest_version
+        );
+        println!("Would you like to update? (y/n)");
+        let mut input = String::new();
+        if let Err(e) = std::io::stdin().read_line(&mut input) {
+            eprintln!("Failed to read input: {}", e);
+            return Ok(());
+        }
+        if input.trim() == "y" {
+            println!("Updating via Homebrew...");
+            run_auto_update().await?;
+        } else if input.trim() == "n" {
+            println!("Update cancelled!");
+            println!("Proceeding to open Stakpak Agent...")
+        } else {
+            println!("Invalid input! Please enter y or n.");
+        }
+    }
+    Ok(())
 }
