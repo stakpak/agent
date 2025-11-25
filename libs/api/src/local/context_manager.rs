87aba43254cfcf7264ce1ce5806623f3b88445a0
George <george@stakpak.dev>
2025-11-25 11:41:42 -0800

    Rename context manager

--- libs/api/src/local/context_manager.rs
+++ libs/api/src/local/context_manager.rs
@@ -1,9 +1,27 @@
-use stakpak_shared::models::integrations::openai::ChatMessage;
+use stakpak_shared::models::{
+    integrations::openai::{ChatMessage, Role},
+    llm::{LLMMessage, LLMMessageContent},
+};
 
-pub fn project_messages(messages: Vec<ChatMessage>) -> String {
-    messages
-        .into_iter()
-        .map(|m| format!("{}: {}", m.role, m.content.unwrap().to_string()))
-        .reduce(|a, b| a + "\n" + &b)
-        .unwrap()
+pub trait ContextManager {
+    fn reduce_context(&self, messages: Vec<ChatMessage>) -> Vec<LLMMessage>;
+}
+
+pub struct SimpleContextManager;
+
+impl ContextManager for SimpleContextManager {
+    fn reduce_context(&self, messages: Vec<ChatMessage>) -> Vec<LLMMessage> {
+        let mut context = Vec::new();
+        context.push(LLMMessage {
+            role: Role::User.to_string(),
+            content: LLMMessageContent::String(
+                messages
+                    .into_iter()
+                    .map(|m| format!("{}: {}", m.role, m.content.unwrap().to_string()))
+                    .reduce(|a, b| a + "\n" + &b)
+                    .unwrap(),
+            ),
+        });
+        context
+    }
 }
